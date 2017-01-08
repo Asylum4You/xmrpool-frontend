d3246e1e1ecfc0172c31302d5f6bd7215695d5aa
Junaos <Junaos@junaos.com>
2017-01-08 17:53:38 -0500

    Added login/logout functionality.

--- app/pool-api/js/miner.js
+++ app/pool-api/js/miner.js
@@ -14,6 +14,7 @@ MinerApi.prototype._update_miner_global_stats = function (miner_address) {
 };
 
 MinerApi.prototype.miner_address = "";
+MinerApi.prototype.miner_address_type = "";
 
 MinerApi.prototype.get_miner_address = function () {
   if (this.miner_address !== "") {
@@ -23,9 +24,39 @@ MinerApi.prototype.get_miner_address = function () {
   }
 };
 
+MinerApi.prototype.set_miner_address = function (address) {
+  if (this.is_valid_btc_address(address)) {
+    this.miner_address = address;
+    this.miner_address_type = "BTC";
+    return true;
+  } else if (this.is_valid_xmr_address(address)) {
+    this.miner_address = address;
+    this.miner_address_type = "XMR";
+    return true;
+  } else {
+    return false;
+  }
+}; 
+
 MinerApi.prototype.refresh_five_seconds = function () {
   this.miner_address = this.get_miner_address();
   if (this.miner_address !== "") {
     this._update_miner_global_stats(this.miner_address);
   }
 };
+
+MinerApi.prototype.is_valid_xmr_address = function (address) {
+  if (address.length == 95 && address.slice(0, 1) == '4') {
+    return true;
+  } else { return false; }
+};
+
+MinerApi.prototype.is_valid_btc_address = function (address) {
+  if (address.length >= 26 && address.length <= 35 && 
+		  (address.slice(0, 1) == '1' ||
+		   address.slice(0, 1) == '3' ||
+		   address.slice(0, 1) == '5')
+      ) {
+    return true;
+  } else { return false; }
+};                                                                                                            
