af03a4c8c7e1c2fc97506f6f8fc42377f2c8c63c
Junaos <Junaos@junaos.com>
2017-01-07 19:48:33 -0500

    Added jumbotron; bug fixes; external libs now on CDN

--- app/pool-api/js/pool-api.js
+++ app/pool-api/js/pool-api.js
@@ -2,26 +2,24 @@
 // Timers for auto-refresh loop
 //
 
+var network_api = {};
+
 $.ajax({
   url: "app/pool-api/js/network.js",
-  dataType: "script"
+  dataType: "script",
+  success: function () {
+    setTimeout(function() { network_api = new NetworkApi(); }, 100);
+  }
 });
 
 $(document).ready(function () {
   $( "#home" ).load( "/app/home.html" );
   $( "#pool-stats" ).load( "/app/pool.html" );
-  var loaded = false;
-  while (!loaded) {
-    try {
-      setTimeout(refreshStats(), 100);
-      loaded = true;
-    } catch(err) {}
-  }
-
+  setTimeout(refreshStats, 500);
 });
 
+
 var refreshStats = function () {
-  var network_api = new NetworkApi();
   network_api._update_network_stats();
 };
 
