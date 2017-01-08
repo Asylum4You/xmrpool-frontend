3796b2e3ff7c05a74644b9a5516131b330ea516b
Junaos <Junaos@junaos.com>
2017-01-07 20:16:48 -0500

    Added significant digits to network block payout; added XMR currency tag

--- app/pool-api/js/network.js
+++ app/pool-api/js/network.js
@@ -13,7 +13,7 @@ NetworkApi.prototype._update_network_stats = function() {
         $('#network-last-block').html(html.height);
         $('#network-difficulty').html(html.difficulty);
         $('#network-last-hash').html(html.hash);
-	$('#network-reward').html(html.value / 1000000000000);
+	$('#network-reward').html((html.value / 1000000000000).toPrecision(13) + " XMR");
 	var t = new Date(html.ts*1000);
         var formatted_ts = t.toISOString();
 	$('#network-timestamp').html(formatted_ts);
