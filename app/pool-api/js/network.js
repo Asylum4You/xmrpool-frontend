31f3c26facf05d65bbc67fdfedcd64237dd78d64
Junaos <Junaos@junaos.com>
2017-01-09 00:32:37 -0500

    Add tooltip to network hash on home page

--- app/pool-api/js/network.js
+++ app/pool-api/js/network.js
@@ -12,7 +12,7 @@ NetworkApi.prototype._update_network_stats = function () {
                 $('#network-last-block').html(html.height);
                 $('#network-difficulty').html(html.difficulty);
                 $('#network-hash-rate').html(hashConversion(Math.floor(html.difficulty / 120)));
-                $('#network-last-hash').html('<a href="http://chainradar.com/xmr/block/' + html.hash + '" target="_blank">' + html.hash.substring(0, 13) + '...</a>');
+                $('#network-last-hash').html('<a href="http://chainradar.com/xmr/block/' + html.hash + '" target="_blank" data-toggle="tooltip" title="' + html.hash + '">' + html.hash.substring(0, 13) + '...</a>');
                 $('#network-reward').html((html.value / 1000000000000).toString().trim('0') + " XMR");
                 $('#network-timestamp').html(timeSince(html.ts*1000) + " ago");
             }
